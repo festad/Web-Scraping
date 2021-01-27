b408c315c91bb1aedde9dfbe68ebfa03796e505a
festad <d.festa004@studenti.unibs.it>
2021-01-27 22:37:19 +0000

    Cda downloader -> learning polish might be easier (hopefully) for now it just retrieves, downloading is the last of the problems

---
+++ cda_downloader.py
@@ -0,0 +1,78 @@
+from downloader import get_soup, download_list_files, get_chrome_driver
+from pathlib import Path
+import os
+import sys
+import re
+from hashlib import sha256
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+p = re.compile(r'folder/[0-9]+')
+pname = re.compile(r'([0-9a-zA-Z]+)')
+
+driver = get_chrome_driver(headless=True)
+
+
+def path_check():
+	os.chdir(Path.home())
+	if 'Cda' not in os.listdir():
+		os.makedirs('Cda')
+	os.chdir('Cda')
+
+
+def download_list_folders(list_folders):
+	k = 0
+	for folder in list_folders:
+		print(f'handling folder -> {folder}')
+		download_folder(folder)
+		
+
+def download_folder(folder):
+	folder_code = sha256(folder.encode('utf-8')).hexdigest()
+	if folder_code not in os.listdir():
+		os.makedirs(folder_code)
+	os.chdir(folder_code)
+	k = 1
+	videos_urls = []
+	videos_names = []
+	while True:
+		soup = get_soup(f'{folder}/{k}')
+		video_tags = soup.find_all('span',\
+			attrs={'class':'wrapper-thumb-link mini-space-bottom hidden-viewList'})
+		if len(video_tags) == 0:
+			break
+		for v in video_tags:
+			video_url = 'https://www.cda.pl' + v.a['href']
+			url_and_name = retrieve_video(video_url)
+			videos_urls.append(url_and_name[0])
+			videos_names.append(url_and_name[1])
+		k += 1
+	for k in range(len(videos_urls)):
+		print(f'{videos_urls[k]} -> {videos_names[k]}')
+	os.chdir('..')
+	
+	
+def retrieve_video(url):
+	driver.get(url)
+	vid_tag = WebDriverWait(driver, 40).until( \
+		EC.presence_of_element_located((By.CLASS_NAME, 'pb-video-player')))
+	video_src = vid_tag.get_attribute('src')
+	soup = get_soup(url)
+	raw_video_name = soup.find(attrs={'property': 'og:title'})['content']
+	video_name = '-'.join(chunk for chunk in pname.findall(raw_video_name)) + f".{video_src.split('.')[-1]}"
+	print(f'{video_name}')
+	return (video_src, video_name)
+	
+
+def strip_code_from_url(folder):
+	return p.match(folder).group(0)[7:]
+	
+	
+if __name__ == '__main__':
+	path_check()
+	if len(sys.argv) < 2:
+		print('Insert cda folder link as an argument')
+		sys.exit()
+	download_list_folders(sys.argv[1:])

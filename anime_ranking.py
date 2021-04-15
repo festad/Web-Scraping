783ff64613f00b456de582b69b8582f7897159bd
festad <d.festa004@studenti.unibs.it>
2021-04-15 11:58:08 +0000

    While cicle

--- anime_ranking.py
+++ anime_ranking.py
@@ -9,7 +9,7 @@ class AnimeRanking:
     def __init__(self):
         self.anime_database = dict()
         self.load_anime_database()
-        self.plot_anime_ranking()
+        # self.plot_anime_ranking()
 
     def add_anime(self, anime_title, anime_score):
         self.anime_database[anime_title] = anime_score
@@ -33,10 +33,35 @@ class AnimeRanking:
         df = pd.DataFrame({'scores': anime_scores},
                           index=anime_titles)
         df = df.sort_values(by=['scores'], ascending=False)
-        df.plot.bar(rot=75, color={'scores': 'red'})
+        df.plot.bar(rot=90, color={'scores': 'red'})
         plt.tight_layout()
         plt.show()
 
 
+def main():
+    a = AnimeRanking()
+    while True:
+        print('1 - Add title and score\n'
+              '2 - Plot\n'
+              '3 - Save\n'
+              'E - Exit')
+        choice = input('-> ')
+        if choice == '1':
+            title = input('Insert title:\n'
+                          '-> ')
+            score = float(input('Insert score [0 - 100]\n'
+                              '-> '))
+            a.add_anime(title, score)
+        elif choice == '2':
+            a.plot_anime_ranking()
+        elif choice == '3':
+            a.save_anime_database()
+        elif choice.lower() == 'e':
+            print('Exit!')
+            exit(0)
+        else:
+            print('Invalid choice!')
+
+
 if __name__ == '__main__':
-    AnimeRanking()
+    main()
